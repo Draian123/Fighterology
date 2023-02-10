3974fe95dcba0968de98993ad62ddf84f93d7c71
Draian123 <beiteldennis3@yahoo.de>
2023-02-10 11:44:48 +0100

    added ejs files

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("profile");
+  res.render("signUp");
 });
+
+router.get("/", (req, res, next) => {
+  res.render("login");
+});
+
 
 module.exports = router;
