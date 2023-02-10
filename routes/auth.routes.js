214754d8c0884effb23184c5731334140c666f5d
Draian123 <beiteldennis3@yahoo.de>
2023-02-10 12:27:40 +0100

    basic routes added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { isLoggedOut, isLoggedIn } = require('../middleware/route-guard');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("signUp");
 });
 
-router.get("/", (req, res, next) => {
+router.post("/", (req, res, next) => {
+
+})
+
+router.get("/login", (req, res, next) => {
   res.render("login");
 });
 
+router.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
+// router.get('/profile', isLoggedIn, async(req, res) => {
+//   const user = req.session.user
+//   res.render('profile', {user})
+// });
 
 module.exports = router;
