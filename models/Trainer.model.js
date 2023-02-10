53acddfd602024c7bba2fcc07049df6b1cd58dc5
Istvan Petrovan <petrovanistvan@gmail.com>
2023-02-10 12:28:42 +0100

    created models

---
+++ models/Trainer.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const trainerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    expertise: {
+      type: String,
+      enum: ['boxing', 'muay thai', 'jiu jitsu', 'wrestling', 'strength']
+    },
+    fighter : {
+        type: [Schema.Types.ObjectId],
+        ref: 'Fighter'
+    },
+    stats: {
+        type: String,
+    },
+  }
+);
+
+const Trainer = model("Trainer", trainerSchema);
+
+module.exports = Trainer;
